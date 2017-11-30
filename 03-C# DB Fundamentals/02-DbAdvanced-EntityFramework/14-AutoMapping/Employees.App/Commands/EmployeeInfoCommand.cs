ffa91af9b48d847cb163e6c35b00234f7f34de78
ruskovweb <ruskovweb@gmail.com>
2017-11-30 16:18:27 +0200

    Added AutoMappingExercise

---
+++ 03-C# DB Fundamentals/02-DbAdvanced-EntityFramework/14-AutoMapping/Employees.App/Commands/EmployeeInfoCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Employees.App.Commands
+{
+    using Employees.Services;
+
+    class EmployeeInfoCommand : ICommand
+    {
+        private readonly EmployeeService employeeService;
+
+        public EmployeeInfoCommand(EmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        public string Execute(params string[] args)
+        {
+            int employeeId = int.Parse(args[0]);
+
+            var employee = employeeService.ById(employeeId);
+
+            return $"ID: {employee.Id} - {employee.FirstName} {employee.LastName} - ${employee.Salary:f2}";
+        }
+    }
+}
---
+++ 03-C# DB Fundamentals/02-DbAdvanced-EntityFramework/14-AutoMapping/Employees.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Employees.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString => @"Server=(LocalDb)\MSSQLLocalDb;Database=EmployeesDb;Integrated Security=True";
+    }
+}
